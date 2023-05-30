acd6c032bd216d5f85084c6c253b6c1a719f692e
unknown <Carlos-Eduardo2000@github.com>
2023-05-30 17:33:38 -0600

    Code complete with comments

--- connectCrazyRadio.py
+++ connectCrazyRadio.py
@@ -1,5 +1,13 @@
+##################################################################################################################################################
+# Autores: Equipo Knight Flight Technologies
+# Fecha de creación: Semana 1  
+# Fecha de última modificación: Semana 1
+# Descripción: Código para conectar y verificar la conexión del dorn con el CrazyRadio
+# Formato de nombrar variables y funciones: Minúsculas y descriptivas
+# Comentado por: Carlos Flores
 
-#codigo para visualizar la conexión con un dron
+##################################################################################################################################################
+
 
 import logging
 import time
@@ -16,14 +24,14 @@ uri = 'radio://0/80/2M/E7E7E7E702'
 
 def simple_connect():
 
-    print("Yeah, I'm connected! :D")
+    print("Yeah, I'm connected! :D") # Mensaje de que se conecta
     time.sleep(3)
-    print("Now I will disconnect :'(")
+    print("Now I will disconnect :'(") #Mensaje para indicar que ha terminado
 
 if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
 
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf: #Se crea una instancia del dron 
 
         simple_connect()
